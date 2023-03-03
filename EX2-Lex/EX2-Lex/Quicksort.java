371e679e9bb793705565bbc5ec465721276093d9
Jayannthan <99635242+Jayannthan@users.noreply.github.com>
2023-03-03 13:14:07 +0530

    Add files via upload

---
+++ EX2-Lex/EX2-Lex/Quicksort.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Quicksort {
+	public static void sort(int[] arr, int low, int high) {
+		if (low < high) {
+			int pivot = partition(arr, low, high);
+			sort(arr, low, pivot - 1);
+			sort(arr, pivot + 1, high);
+		}
+	}
+
+	private static int partition(int[] arr, int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
+		for (int j = low; j < high; j++) {
+			if (arr[j] < pivot) {
+				i++;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+		return i + 1;
+	}
+
+	public static void main(String[] args) {
+
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of elements you want to store: ");
+
+		n = sc.nextInt();
+
+		int[] array = new int[n];
+		System.out.println("Enter the elements of the array: ");
+		for (int i = 0; i < n; i++) {
+
+			array[i] = sc.nextInt();
+		}
+
+		sort(array, 0, array.length - 1);
+		System.out.print("sorted: ");
+
+		for (int i : array) {
+			System.out.print(i + " ");
+		}
+	}
+}
